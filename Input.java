4b6ff55aadb3513eebcc381348eec356096ba311
Lewis Barker <lewisjbarker@btopenworld.com>
2013-02-07 20:05:01 +0000

    Added input
    Exiting game through esc works, but I wasn't sure where to define downPressed etc so they don't work yet.

--- Input.java
+++ Input.java
@@ -13,12 +13,38 @@ public class Input implements KeyListener{
 
     //key class, do not rename/remove
     public void keyPressed(KeyEvent e) {
-
+    	if (e.getKeyCode() == KeyEvent.VK_UP) {
+                upPressed = true;
+            }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                downPressed = true;
+            }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                upPressed = true;
+            }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                downPressed = true;
+            }
     } 
     //key class, do not rename/remove
-    public void keyReleased(KeyEvent e) {			
+    public void keyReleased(KeyEvent e) {
+    	if (e.getKeyCode() == KeyEvent.VK_UP) {
+                upPressed = false;
+            }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                downPressed = false;
+            }
+         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            	upPressed = false;
+            }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            	downPressed = false;
+            }	
     }
     //key class, do not rename/remove
     public void keyTyped(KeyEvent e) {
+    	if (e.getKeyChar() == 27) {
+		System.exit(0);
+    	}
     }
 };
