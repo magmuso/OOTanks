d9155c51f7e4812de684e5efe9c56cfdd632522f
magniffico <martynas.melninkas@gmail.com>
2013-02-16 14:21:14 +0000

    Merged how everything should be

--- Land.java
+++ Land.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 
 public class Land {
-<<<<<<< HEAD
 	public final int width;
 	public final int height;
 	
@@ -16,17 +15,4 @@ public class Land {
 	public void createTank(double x, double y, int ctrl){
 		tanks.add(new Tank(x, y, ctrl));
 	}
-=======
-    public ArrayList<Tank> tanks;
-    public final int width;
-    public final int height;
-    public Land(int in_w, int in_h){
-	width = in_w;
-	height = in_h;
-	tanks = new ArrayList();
-    }
-    public void createTank(double x, double y, int ctrl){
-	tanks.add(new Tank(x,y,ctrl));
-    }
->>>>>>> e44a3a89dff492a4b9a0440365b60d5c89ed58e7
 }
