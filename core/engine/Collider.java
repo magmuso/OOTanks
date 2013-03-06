8787a7bf97e949ea21b48328fbb874b94330f9e2
= <=>
2013-03-06 16:58:16 +0000

    More vector calculation, some calculation of collision detection

--- core/engine/Collider.java
+++ core/engine/Collider.java
@@ -7,33 +7,38 @@ public class Collider{
 		this.map = map;
 	}
 	public boolean update(){
-		return outOfBounds();
+		return false;
 	}
-	//brute force collision detection
-	private boolean outOfBounds(){
-		for(GameEntity gEnt: map.gameEntities){
-			Vector2D tRC = Vector2D.transform(new Vector2D(gEnt.getWidth()/2,gEnt.getHeight()/2),Vector2D.rotationMatrix(gEnt.getAngle()));
-			Vector2D tLC = Vector2D.transform(new Vector2D(-gEnt.getWidth()/2,gEnt.getHeight()/2),Vector2D.rotationMatrix(gEnt.getAngle()));
-			Vector2D bRC = Vector2D.transform(new Vector2D(gEnt.getWidth()/2,-gEnt.getHeight()/2),Vector2D.rotationMatrix(gEnt.getAngle()));
-			Vector2D bLC = Vector2D.transform(new Vector2D(-gEnt.getWidth()/2,-gEnt.getHeight()/2),Vector2D.rotationMatrix(gEnt.getAngle()));
-			if (tRC.x+gEnt.getX() < 0) { return true; }
-			if (tRC.x+gEnt.getX() > 800) { return true; }
-			if (tRC.y+gEnt.getY() < 0) { return true; }
-			if (tRC.y+gEnt.getY() > 600) { return true; }
-			if (tLC.x+gEnt.getX() < 0) { return true; }
-			if (tLC.x+gEnt.getX() > 800) { return true; }
-			if (tLC.y+gEnt.getY() < 0) { return true; }
-			if (tLC.y+gEnt.getY() > 600) { return true; }
-			if (bLC.x+gEnt.getX() < 0) { return true; }
-			if (bLC.x+gEnt.getX() > 800) { return true; }
-			if (bLC.y+gEnt.getY() < 0) { return true; }
-			if (bLC.y+gEnt.getY() > 600) { return true; }
-			if (bRC.x+gEnt.getX() < 0) { return true; }
-			if (bRC.x+gEnt.getX() > 800) { return true; }
-			if (bRC.y+gEnt.getY() < 0) { return true; }
-			if (bRC.y+gEnt.getY() > 600) { return true; }
-			return false;
-		}
+	public boolean collides(GameEntity rec1, GameEntity rec2){
+		//We get relevant corners with 0, 0 as origin
+		Vector2D tl1 = new Vector2D(-rec1.getWidth()/2, -rec1.getHeight()/2);
+		Vector2D tr1 = new Vector2D(tl1.x + rec1.getWidth(), tl1.y);
+		Vector2D br1 = new Vector2D(tl1.x + rec1.getWidth(), tl1.y + rec1.getHeight());
+
+		Vector2D tl2 = new Vector2D(-rec2.getWidth()/2, -rec2.getHeight()/2);
+		Vector2D tr2 = new Vector2D(tl2.x + rec2.getWidth(), tl2.y);
+		Vector2D bl2 = new Vector2D(tl2.x, tl2.y + rec2.getHeight());
+		
+		//we rotate the corners and add
+		tl1 = Vector2D.add(Vector2D.rotate(tl1, rec1.getAngle()),new Vector2D(rec1.getX(),rec1.getY()));
+		tr1 = Vector2D.add(Vector2D.rotate(tr1, rec1.getAngle()),new Vector2D(rec1.getX(),rec1.getY()));
+		br1 = Vector2D.add(Vector2D.rotate(br1, rec1.getAngle()),new Vector2D(rec1.getX(),rec1.getY()));
+		
+		tl2 = Vector2D.add(Vector2D.rotate(tl2, rec2.getAngle()),new Vector2D(rec2.getX(),rec2.getY()));
+		tr2 = Vector2D.add(Vector2D.rotate(tr2, rec2.getAngle()),new Vector2D(rec2.getX(),rec2.getY()));
+		bl2 = Vector2D.add(Vector2D.rotate(bl2, rec2.getAngle()),new Vector2D(rec2.getX(),rec2.getY()));
+		
+		Vector2D axis1 = Vector2D.sub(tr1, tl1);
+		Vector2D axis2 = Vector2D.sub(tr1, br1);
+		Vector2D axis3 = Vector2D.sub(tl2, bl2);
+		Vector2D axis4 = Vector2D.sub(tl2, tr2);
+		
+		double max11 = Vector2D.dot(Vector2D.proj(tr1, axis1),axis1);
+		double min11 = Vector2D.dot(Vector2D.proj(tl1, axis1),axis1);
+		
+		double max12 = Vector2D.dot(Vector2D.proj(tr2, axis1), axis1);
+		double min12 = Vector2D.dot(Vector2D.proj(tl2, axis1), axis1);
+		//check axis 2 with 3 & 4;
 		return false;
 	}
 };
--- core/engine/Vector2D.java
+++ core/engine/Vector2D.java
@@ -7,6 +7,10 @@ public class Vector2D {
 		this.x = x;
 		this.y = y;
 	}
+	public Vector2D(int x, int y){
+		this.x = (double)x;
+		this.y = (double)y;
+	}
 	public static Vector2D transform(Vector2D point, double trans[][]){
 		Vector2D result = new Vector2D(0,0);
 		result.x = trans[0][0]*point.x + trans[0][1]*point.y;
@@ -21,13 +25,22 @@ public class Vector2D {
 		result [1][1] = Math.cos(angle);
 		return result;
 	}
+	public static Vector2D proj(Vector2D v, Vector2D onTo){
+		return times(onTo, dot(v,onTo)/dot(onTo,onTo));
+	}
 	public static Vector2D add(Vector2D v1, Vector2D v2){
 		return new Vector2D (v1.x+v2.x, v1.y+v2.y);
 	}
-	public static Vector2D dot(Vector2D v1, Vector2D v2){
-		return new Vector2D (v1.x*v2.x, v1.y*v2.y);
+	public static Vector2D sub(Vector2D v1, Vector2D v2){
+		return new Vector2D(v1.x-v2.x, v1.y-v2.y);
+	}
+	public static double dot(Vector2D v1, Vector2D v2){
+		return v1.x*v2.x + v1.y*v2.y;
 	}
 	public static Vector2D times(Vector2D v, double t){
 		return new Vector2D (v.x*t, v.y*t);
+	}
+	public static Vector2D rotate(Vector2D v, double angle){
+		return transform(v,rotationMatrix(angle));
 	}
 }
