7993ed9c4cf2c40be8bc68479f848006131b8859
Manas Bajaj <manas_bajaj007@hotmail.com>
2013-03-07 19:55:39 +0000

    Sprite class updated + New tank images added + New terrain added

--- graphics/Sprite.java
+++ graphics/Sprite.java
@@ -1,7 +1,23 @@
 package graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 public class Sprite{
-	public Sprite(){
+	
+	private Image pic;
 
+	//CONSTRUCTOR
+	public Sprite(String path){
+		pic = new ImageIcon(path).getImage();
+	}
+	
+	public void draw(Graphics2D g, int x, int y,double angle){
+		g.translate(x, y);
+		g.rotate(angle);
+		g.drawImage(pic, x, y,null);
+		g.rotate(-angle);
+		g.translate(-x,-y);
+		
 	}
 };
